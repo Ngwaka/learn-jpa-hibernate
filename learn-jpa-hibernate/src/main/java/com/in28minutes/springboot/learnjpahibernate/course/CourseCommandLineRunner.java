37233bc97a7a99fb8f397df32cec64b4397962f1
Claude Ekango Ngwaka <claude.ngwaka@alten.it>
2023-03-28 22:08:04 +0200

    159.step10-getting started with Spring Data JPA

--- learn-jpa-hibernate/src/main/java/com/in28minutes/springboot/learnjpahibernate/course/CourseCommandLineRunner.java
+++ learn-jpa-hibernate/src/main/java/com/in28minutes/springboot/learnjpahibernate/course/CourseCommandLineRunner.java
@@ -7,34 +7,50 @@ import org.springframework.stereotype.Component;
 
 import com.in28minutes.springboot.learnjpahibernate.course.jdbc.CourseJdbcRepository;
 import com.in28minutes.springboot.learnjpahibernate.course.jpa.CourseJpaRepository;
+import com.in28minutes.springboot.learnjpahibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 
 
 @Component
 public class CourseCommandLineRunner implements CommandLineRunner{
 	
-//	@Autowired
-//	private CourseJdbcRepository JDBCrepository ;  // persistance avec JDBC
-
 	@Autowired
-	private CourseJpaRepository repository ;   // persistance avec jpa
+	private CourseSpringDataJpaRepository repository ;
+
+//	@Autowired
+//	private CourseJpaRepository jpaRepository ;   // persistance avec jpa
+	
+	
+//	@Autowired
+//	private CourseJdbcRepository jdbcRepository ;  // persistance avec JDBC
+
 	
 	@Override
 	public void run(String... args) throws Exception {
-		repository.insert(new Course(1 , "Learn AWS NOW JPA", "in28minutes"));
-		repository.insert(new Course(2 , "Learn Azure Now JPA", "in28minutes"));
-		repository.insert(new Course(3 , "Learn DevOps Now! JPA", "in28minutes"));
-		
-		repository.deleteById(1);
-		System.out.println(repository.findById(3));
 		
 		
+		repository.save(new Course(1 , "Learn AWS NOW springDataJpa", "in28minutes"));
+		repository.save(new Course(2 , "Learn Azure Now springDataJpa", "in28minutes"));
+		repository.save(new Course(3 , "Learn DevOps Now! springDataJpa", "in28minutes"));
+		repository.deleteById(1l);
+		System.out.println(repository.findById(3l));
+	
 		
-	//	JDBCrepository.insert(new Course(4 , "Learn AWS NOW JDBC", "in28minutes"));
-	//	JDBCrepository.insert(new Course(5 , "Learn Azure Now JDBC", "in28minutes"));
-	//	JDBCrepository.insert(new Course(6 , "Learn DevOps Now! JDBC", "in28minutes"));
 		
-	//	JDBCrepository.deleteById(4);
-	//	System.out.println(JDBCrepository.findById(6));
+	
+	//	jpaRepository.insert(new Course(1 , "Learn AWS NOW JPA", "in28minutes"));
+	//	jpaRepository.insert(new Course(2 , "Learn Azure Now JPA", "in28minutes"));
+	//	jpaRepository.insert(new Course(3 , "Learn DevOps Now! JPA", "in28minutes"));
+	//	jpaRepository.deleteById(1);
+	//	System.out.println(jpaRepository.findById(3));
+		
+		
+
+		
+	//	jdbcRepository.insert(new Course(4 , "Learn AWS NOW JDBC", "in28minutes"));
+	//	jdbcRepository.insert(new Course(5 , "Learn Azure Now JDBC", "in28minutes"));
+	//	jdbcRepository.insert(new Course(6 , "Learn DevOps Now! JDBC", "in28minutes"));
+	//	jdbcRepository.deleteById(4);
+	//	System.out.println(jdbcRepository.findById(6));
 		
 	}
 
